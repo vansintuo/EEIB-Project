f69c093ce93a9d4f73f147193043c7ca11e7e49b
Tuo Vansin <tuovansin123@gmail.com>
2024-09-11 15:47:37 +0700

    first commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            Dashboard
+        </template>
+
+        <div class="p-4 bg-white rounded-lg shadow-xs">
+            You're logged in!
+        </div>
+        <span class="p-4 bg-warning h4">This page for vue Dashboard</span>
+    </AuthenticatedLayout>
+
+</template>
